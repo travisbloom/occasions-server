f502d533304f3ed50ab341756c26cb5fb1356032
Travis Bloom <travisbloom@gmail.com>
2017-05-22 22:02:03 -0400

    test fixes

--- events/management/commands/test_generate_events_next_date.py
+++ events/management/commands/test_generate_events_next_date.py
@@ -12,6 +12,7 @@ class GenerateEventsNextDateCommandTestCase(BaseTestCase):
         self.date_to_update_off = self.base_date.add(days=3)
         for _ in range(10):
             self.event_to_update = EventFactory(
+                post__has_event_type=False,
                 post__has_event_date=False,
                 is_reoccuring_yearly=True,
                 is_default_event=False
@@ -29,6 +30,7 @@ class GenerateEventsNextDateCommandTestCase(BaseTestCase):
                 date_start=self.base_date.subtract(years=2, days=3)
             )
         self.event_without_update = EventFactory(
+            post__has_event_type=False,
             post__has_event_date=False,
             is_reoccuring_yearly=True,
             is_default_event=False
